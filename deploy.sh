1e7c9f0b3d6f356f86e13eba9480e0f74949ea59
Daniel Ionut <danutu@pop-os.localdomain>
2020-01-29 20:18:02 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ git pull origin master
 sudo kill -9 $(lsof -t -i:3000)
 npm install --save
 npm run build
-nohup npm run start >/dev/null 2>&1 &
-slper 50
+sudo systemctl start newapp-svelte &
 exit 0
