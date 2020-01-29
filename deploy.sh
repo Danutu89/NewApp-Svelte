9b7975aee9ab5db14b7776ad43d80224bc538279
Daniel Ionut <danutu@pop-os.localdomain>
2020-01-29 18:22:16 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/expect -f
-cd /home/newapp/newappv2
-git pull
+#!/bin/bash
+cd /home/newapp/newappv2/
+git pull origin master
 sudo kill -9 $(lsof -t -i:3000)
 npm run build
 nohup npm run start
