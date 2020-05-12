cf51edc3b22ee80e18a67bfc3e6880db92748b76
Danutu <danutu@newapp.nl>
2020-05-12 17:44:31 +0200

    update

--- src/components/Controllers/Swipe.svelte
+++ src/components/Controllers/Swipe.svelte
@@ -1,16 +1,17 @@
 <script>
-import { onMount } from 'svelte';
+import { onMount, beforeUpdate } from 'svelte';
+import { lPage } from '../../modules/Preloads.js';
 
 let swipeDir = "none";
 let elementSwiped = "none";
 let touchStart, touchCurrent, touchEnd;
 let touchLive = [];
 let touchPosStart;
-let swipeThreshold = 20;
-let swipeLenght = 1;
+let swipeThreshold = 4;
 let touchThreshold = 10;
-let touchDistance = 3;
+let touchDistance = 5;
 let change;
+let elementOpened = "none";
 
 let overflow;
 
@@ -32,7 +33,10 @@ function swipeStart(e){
     if(typeof(sidebar) != "undefined"){
         sidebar.style.transition = "none";
     }
-
+    if(typeof(reload) != "undefined"){
+        reload.style.transition = "none";
+    }
+    document.body.style.overflow = 'hidden';
     touchLive.push(touchStart);
 }
 
@@ -44,13 +48,13 @@ function swipeMove(e){
 
     change = {x: (touchStart.x - touchCurrent.x)*-1, y: (touchStart.y - touchCurrent.y)*-1};
 
-    if(typeof(touchLive[touchLive.length - touchDistance]) == "undefined"){
+    if(typeof(touchLive[touchLive.length - touchDistance]) != "object"){
         touchLive.push(touchCurrent);
         swipeDir = "none";
+        //touchStart = touchCurrent;
         return;
     }
-
-    if (Math.abs(touchCurrent.x - touchLive[touchLive.length - touchDistance].x) > swipeLenght && Math.abs(touchCurrent.y - touchLive[touchLive.length - touchDistance].y) < swipeThreshold){
+    if (Math.abs(touchCurrent.x - touchLive[touchLive.length - touchDistance].x) > swipeThreshold && Math.abs(touchCurrent.y - touchLive[touchLive.length - touchDistance].y) < swipeThreshold){
         if(touchCurrent.x - touchLive[touchLive.length - touchDistance].x < 0){
             if(swipeDir != "left"){
                 touchLive = [];
@@ -64,7 +68,7 @@ function swipeMove(e){
             swipeDir = "right";
             touchLive.push(touchCurrent);
         }
-    }else if(Math.abs(touchCurrent.y - touchLive[touchLive.length - touchDistance].y) > swipeLenght && Math.abs(touchCurrent.x - touchLive[touchLive.length - touchDistance].x) < swipeThreshold){
+    }else if(Math.abs(touchCurrent.y - touchLive[touchLive.length - touchDistance].y) > swipeThreshold && Math.abs(touchCurrent.x - touchLive[touchLive.length - touchDistance].x) < swipeThreshold){
         if(touchCurrent.y - touchLive[touchLive.length - touchDistance].y < 0){
             if(swipeDir != "up"){
                 touchLive = [];
@@ -82,20 +86,21 @@ function swipeMove(e){
         swipeDir = "none";
         touchLive = [];
         touchLive.push(touchCurrent);
+        return;
     }
 
-    if(swipeDir == "right" && touchPosStart.x < window.innerWidth/4){
-        if(typeof(sidebar) != "undefined"){
+    console.log(swipeDir);
+
+    if(swipeDir == "right" && touchPosStart.x < window.innerWidth/4 || swipeDir == "left" && elementOpened == "wrapper"){
+        if(typeof(sidebar) != "undefined"&& (elementSwiped == "wrapper" || elementSwiped == "none") && (elementOpened == "wrapper" || elementOpened == "none")){
             elementSwiped = "wrapper";
+            elementOpened = "wrapper";
             dragWrapper(e);
         }
     }else if(swipeDir == "left"){
-        if(typeof(sidebar) != "undefined" && elementSwiped == "wrapper" || sidebarOpened){
-            dragWrapper(e);
-            elementSwiped = "wrapper";
-        }
+        
     }else if(touchPosStart.y < window.innerHeight/5 && document.body.scrollTop == 0 && window.scrollY == 0 && swipeDir == "down" || swipeDir == "up"){
-        if(typeof(reload) != "undefined"){
+        if(typeof(reload) != "undefined" && (elementOpened == "none" || elementOpened == "reload") && (elementSwiped == "none" || elementSwiped == "reload")){
             elementSwiped = "reload";
             dragReload(e);
         }
@@ -104,9 +109,11 @@ function swipeMove(e){
     }else{
         elementSwiped = "none";
     }
+    //console.log(touchStart);
+    //console.log(swipeDir, touchLive);
 }
 
-function swipeEnd(e){
+async function swipeEnd(e){
     if(typeof(e.targetTouches[0]) == "undefined")
         touchEnd = {x: e.changedTouches[0].screenX, y: e.changedTouches[0].screenY};
     else
@@ -117,7 +124,7 @@ function swipeEnd(e){
     if(isTouch)
         swipeDir = "touch";
 
-    if(typeof(sidebar) != "undefined" && !isTouch){
+    if(typeof(sidebar) != "undefined" && !isTouch && elementSwiped == "wrapper" || elementOpened == "wrapper"){
         sidebar.style.transition = "";
         var tranX = sidebar.style.transform;
         tranX = tranX.replace(/[^\d.]/g, '');
@@ -125,21 +132,50 @@ function swipeEnd(e){
         if(tranX != 20 && tranX != 290){
             if( Math.abs(tranX - 20) > Math.abs(tranX - 290)){
                 sidebar.style.transform = "translateX(-290px)";
-                sidebarOpened = false;
+                elementOpened = "none";
                 overflow.classList.remove("show");
             }else{
                 sidebar.style.transform = "translateX(-20px)";
-                sidebarOpened = true;
+                elementOpened = "wrapper";
                 overflow.classList.add("show");
             }
         }
         //console.log(Math.abs(tranX - 20),Math.abs(tranX - 290))
     }
+
+    if(elementOpened != "wrapper")
+        document.body.style.overflow = 'visible';
+
+    if(typeof(reload) != "undefined" && !isTouch && elementSwiped == "reload" || elementOpened == "reload"){
+        reload.style.transition = "";
+
+        if(change.y > 100){
+            reload.style["min-height"] = 'calc('+reloadHeight+'px + 60px)';
+            reload.style.transform = 'translateY(60px)';
+            reload.children[0].style["-webkit-animation"] = "load8 1.1s infinite linear";
+            reload.children[0].style["animation"] = "load8 1.1s infinite linear";
+            //loading = true;
+            elementOpened = "reload";
+            await setTimeout(()=>{
+                lPage.set({data: $lPage.data, refresh: true});
+            }, 400)
+        }else{
+            reload.style["min-height"] = '';
+            reload.style.transform = '';
+            reload.children[0].style.display = 'none';
+			reload.children[0].style.transform = '';
+			reload.children[0].style["-webkit-animation"] = "";
+			reload.children[0].style["animation"] = "";
+            elementOpened = "none";
+        }
+    }
+    //console.log(change)
     //console.log(swipeDir, elementSwiped);
     //console.log(isTouch);
     touchLive = [];
     swipeDir = "none";
     elementSwiped = "none";
+    change = {};
 }
 
 function dragWrapper(e){
@@ -165,7 +201,8 @@ function dragReload(e){
 	if(document.body.scrollTop == 0 && window.scrollY == 0){
 		document.body.style.overflow = 'hidden';
 	
-		reload.children[0].style.display = 'block';
+        reload.children[0].style.display = 'block';
+        elementOpened = "reload";
 
 		if(change.y < 70 ){
             reload.style.transform = 'translateY('+(change.y+30)+'px)';
@@ -178,13 +215,39 @@ function dragReload(e){
         }
         
     }
+    //console.log(change);
     
 }
+
+async function resetLoader(){
+	await setTimeout(()=>{
+        reload.style["min-height"] = '';
+	    reload.style.transform = '';
+		reload.children[0].style.display = 'none';
+		reload.children[0].style.transform = '';
+		reload.children[0].style["-webkit-animation"] = "";
+        reload.children[0].style["animation"] = "";
+        elementOpened = "none";
+	},400)
+}
+
+beforeUpdate(()=>{
+    overflow = document.querySelector("overflow");
+
+    //wrapper req
+    sidebar = document.getElementById("sidebar-left");
+    navbar = document.querySelector(".newapp-navbar");
+
+    //reload req
+    reload = document.querySelector("reload");
+})
 
 onMount(()=>{
     document.addEventListener("touchstart", swipeStart);
     document.addEventListener("touchmove", swipeMove);
     document.addEventListener("touchend", swipeEnd);
+
+    document.addEventListener('reloaded', resetLoader);
 
     overflow = document.querySelector("overflow");
 
@@ -194,7 +257,7 @@ onMount(()=>{
 
     //reload req
     reload = document.querySelector("reload");
-    reloadHeight = parseFloat((getComputedStyle(reload).minHeight).replace(/[^\d.]/g, ''));;
+    reloadHeight = parseFloat((getComputedStyle(reload).minHeight).replace(/[^\d.]/g, ''));
 
 });
 </script>
